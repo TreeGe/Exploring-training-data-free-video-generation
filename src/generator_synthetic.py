1fe7947dab2311889e8928cf205274c515cd919b
shenghuayong <126758087+TreeGe@users.noreply.github.com>
2025-05-26 09:58:48 +0800

    Update generator_synthetic.py

--- src/generator_synthetic.py
+++ src/generator_synthetic.py
@@ -1,70 +1,95 @@
 import os
-
 import argparse
 import torch
-
+import imageio
+from PIL import Image
 from diffusers import DDIMScheduler
-from src.utils.edit_directions import construct_direction
-from src.edit_pipeline_inter import EditingPipeline
+from edit_pipeline_inter_flow import EditingPipeline
+from utils.ddim_inv import DDIMInversion
+from utils.scheduler import DDIMInverseScheduler
 
-# if torch.cuda.is_available():
-#     device = "cuda"
-# else:
-device = "cuda"
+if torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
 
 
-if __name__=="__main__":
+def load_sentence_embeddings(l_sentences, tokenizer, text_encoder, device=device):
+    with torch.no_grad():
+        l_embeddings = []
+        for sent in l_sentences:
+            text_inputs = tokenizer(
+                sent,
+                padding="max_length",
+                max_length=tokenizer.model_max_length,
+                truncation=True,
+                return_tensors="pt",
+            )
+            text_input_ids = text_inputs.input_ids
+            prompt_embeds = text_encoder(text_input_ids.to(device), attention_mask=None)[0]
+            l_embeddings.append(prompt_embeds)
+    return torch.cat(l_embeddings, dim=0).mean(dim=0).unsqueeze(0)
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--prompt_str', type=str, default='The mountain at sunrise')
-    parser.add_argument('--random_seed', default=7)
-    parser.add_argument('--task_name', type=str, default='The mountain at sunrise2The mountain at sunse')
-    parser.add_argument('--results_folder', type=str, default='output/test')
     parser.add_argument('--num_ddim_steps', type=int, default=50)
     parser.add_argument('--model_path', type=str, default='model_path')
+    parser.add_argument('--random_seed', default=1)
     parser.add_argument('--xa_guidance', default=0.1, type=float)
     parser.add_argument('--negative_guidance_scale', default=5.0, type=float)
     parser.add_argument('--use_float_16', action='store_true')
-    parser.add_argument('--samples_times', type=int ,default=10)
+    parser.add_argument('--samples_times', type=int, default=6)
+
+    parser.add_argument('--input_img', default="output/test/real_image/girffe.png")
+    parser.add_argument('--input_sentence', default="a giraffe standing next to a lush green field")
+    parser.add_argument('--input', default="noon")
+    parser.add_argument('--output', default="night")
+
+
     args = parser.parse_args()
 
-    os.makedirs(args.results_folder, exist_ok=True)
+    os.makedirs(os.path.join(args.results_folder, "edit"), exist_ok=True)
+    os.makedirs(os.path.join(args.results_folder, "reconstruction"), exist_ok=True)
 
     if args.use_float_16:
         torch_dtype = torch.float16
     else:
         torch_dtype = torch.float32
 
-    # make the input noise map
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed(args.random_seed)  #设置随机种子，让每次生成的随机种子都相同
-    else:
-        torch.manual_seed(args.random_seed)
+    pipe1 = DDIMInversion.from_pretrained(args.model_path, torch_dtype=torch_dtype).to(device)
+    pipe1.scheduler = DDIMInverseScheduler.from_config(pipe1.scheduler.config)
 
-    x = torch.randn((1,4,64,64), device=device)
-    x_1 = torch.randn((1,4,64,64), device=device)
+    img = Image.open(args.input_img)
+    x_inv, x_inv_image, x_dec_img = pipe1(
+        args.input_sentence,
+        guidance_scale=1,
+        num_inversion_steps=args.num_ddim_steps,
+        img=img,
+        torch_dtype=torch_dtype
+    )
 
-    # Make the editing pipeline
     pipe = EditingPipeline.from_pretrained(args.model_path, torch_dtype=torch_dtype).to(device)
     pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config)
     pipe.safety_checker = lambda images, clip_input: (images, False)
 
 
-    rec_pil, edit_pil= pipe(args.prompt_str,
-        num_inference_steps=args.num_ddim_steps,
-        x_in=x,
-        # x_1 = x_1,
-        edit_dir=construct_direction(args.task_name),
-        guidance_amount=args.xa_guidance,
-        guidance_scale=args.negative_guidance_scale,
-        negative_prompt="", # use the empty string for the negative prompt
-        samples_times=args.samples_times
-    )
+    input_mean_emb = load_sentence_embeddings([args.input], pipe.tokenizer, pipe.text_encoder, device='cuda')
+    output_mean_emb = load_sentence_embeddings([args.output], pipe.tokenizer, pipe.text_encoder, device='cuda')
 
-    rec_pil[0].save(f"output/test/9.9/{args.random_seed}_1.png")
-    for i in range(0,args.samples_times):        
-        (edit_pil[i])[0].save(f"output/test/9.9/{args.random_seed}{i}_1.png")
-    
-    
-    # (edit_pil[0])[0].save(os.path.join(args.results_folder, f"edit_01.png"))
-    # rec_pil[0].save(os.path.join(args.results_folder, f"reconstruction_01.png"))
-    # res[0].save(os.path.join(args.results_folder, f"res_1.png"))
+    edit_dir = ((output_mean_emb.mean(0) - input_mean_emb.mean(0)).unsqueeze(0))
+    rec_pil, edit_pil, edit_inter = pipe(args.input_sentence,
+                                         num_inference_steps=args.num_ddim_steps,
+                                         x_in=x_inv,
+                                         edit_dir=edit_dir,
+                                         guidance_amount=args.xa_guidance,
+                                         guidance_scale=args.negative_guidance_scale,
+                                          negative_prompt=args.input_sentence,
+                                         samples_times=args.samples_times
+                                         )
+
+    rec_pil[0].save(f"args.results_folder/rec.png")
+    for i in range(0, args.samples_times):
+        (edit_pil[i]).save(os.path.join(f"args.results_folder/rec.png"))
+
+    imageio.mimsave(f'args.results_folder/gene_default.gif', 'GIF', duration=250)
